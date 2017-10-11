5f8783ea6e00f6981ab8e1631f0ff8d9d36038dd
tushar.cseru09 <tushar.cseru09@gmail.com>
2017-10-12 00:38:57 +0600

    jaxRs resource life cycle. Singleton example.

--- src/main/java/com/cseru09/Services/Service.java
+++ src/main/java/com/cseru09/Services/Service.java
@@ -1,18 +1,22 @@
 package com.cseru09.Services;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("test")
+@Singleton
 public class Service {
+	
+	private int Count;
 	
 	//http://localhost:8989/JAX-RS/rest/test
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getTestmessage(){
-		
-		return "Testing message!";
+		Count = Count + 1;
+		return "Testing message! Requested "+Count+ " time(s).";
 	}
 }
